69d0635df3afc5b07809d5e70f51be610340bd92
Lucas Rennó Kallás <lucasrk@unifei.edu.br>
2020-08-08 18:36:55 -0300

    some fixes

--- web/src/pages/Landing/Landing.tsx
+++ web/src/pages/Landing/Landing.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import backgroundImg from '../../assets/images/background.png'
-
+import {Link} from 'react-router-dom';
 import './styles.css'
 import PageHeader from '../../components/PageHeader/PageHeader';
+import penImg from '../../assets/images/pen.png'
 
 function Landing() {
   return (
@@ -10,7 +11,10 @@ function Landing() {
       <PageHeader inputBoolean={false} ></PageHeader>
       <div className="content">
         <img src={backgroundImg} alt="Leaf and Books" className="backgroundImg"/>
-        <button type="button">New file</button>
+        <Link to="\writer" className="writeBut">
+          <img src={penImg} alt="new-file"></img>
+          New file
+        </Link>
       </div>
       <a href="https://github.com/lucasrennok" target="_blank" >
         <h2 className="title-footer">lucasrennok</h2>
