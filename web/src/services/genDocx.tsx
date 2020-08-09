07bd06ec174228f74d4b7af20fbcb142519722ff
Lucas Rennó Kallás <lucasrk@unifei.edu.br>
2020-08-09 16:07:49 -0300

    gen doc

--- web/src/services/genDocx.tsx
+++ web/src/services/genDocx.tsx
@@ -2,7 +2,16 @@
 //And gen a docx with the data
 //After that the docx will be downloaded
 
+import {Packer, AlignmentType, Document, HeadingLevel, Paragraph, TabStopPosition, TabStopType, TextRun } from 'docx';
+import {saveAs} from 'file-saver';
+
 export function genDocxWordWithData(fileName: string, textWrote: string){
     console.log(fileName, textWrote);
+    const doc = new Document();
 
+    Packer.toBlob(doc).then(blob => {
+      console.log(blob);
+      saveAs(blob, "test.docx");
+      console.log("Document created successfully");
+    });
 }
