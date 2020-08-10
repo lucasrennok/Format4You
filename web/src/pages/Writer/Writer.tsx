24ee6a0c6859a0eee9f4979841a2e977612de903
Lucas Rennó Kallás <lucasrk@unifei.edu.br>
2020-08-10 01:51:12 -0300

    placeholders

--- web/src/pages/Writer/Writer.tsx
+++ web/src/pages/Writer/Writer.tsx
@@ -6,12 +6,17 @@ import PageFooter from '../../components/PageFooter/PageFooter';
 import { genDocxWordWithData } from '../../services/genDocx';
 
 function Writer() {
+  // defaultTextArea => template SBC
   const defaultTextArea = "#title:\n#author:\n#institute:\n#email:\n#abstract:\n#resumo:\n#:\n#:\n#section:\n#:\n#text:\n#:";
   const [fileName, setFileName] = useState('newDocument');
   const [textInWriter, setTextInWriter] = useState('');
 
   const handleWriterChange = (e: React.ChangeEvent<HTMLTextAreaElement>) =>{
     setTextInWriter(e.target.value);
+    //e.target.style.height = '100rem';
+    // if(e.target.scrollHeight !== e.target.style.height){
+    //   console.log(e.target.scrollHeight);
+    // }
   }
   
   const handleDocTitleChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
@@ -28,14 +33,14 @@ function Writer() {
       <div className="inputTitle">
         <label>Document title</label>
         <div className="inputBox">
-          <input type="text" maxLength={15} defaultValue="newDocument" onChange={handleDocTitleChange} />
+          <input placeholder="Type a title" type="text" maxLength={15} defaultValue="newDocument" onChange={handleDocTitleChange} />
           <p>.docx</p>
         </div>
       </div>
 
       <div className="content">
         <h1 className="title-writer">Writer</h1>
-        <textarea defaultValue={defaultTextArea} onChange={handleWriterChange}/>
+        <textarea placeholder="Type something" defaultValue={defaultTextArea} onChange={handleWriterChange} className="autoTextArea" />
         <button onClick={getData} type="button">Format</button>
       </div>
       <PageFooter />
