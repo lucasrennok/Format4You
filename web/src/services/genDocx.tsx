9e7f6ac6382e20c94c54e173def7deb0c46fef9e
Lucas Rennó Kallás <lucasrk@unifei.edu.br>
2020-08-09 21:34:28 -0300

    starting with the data

--- web/src/services/genDocx.tsx
+++ web/src/services/genDocx.tsx
@@ -1,20 +1,30 @@
-//Here I will convert the data from the writer
-//And gen a docx with the data
-//After that the docx will be downloaded
-
-import {Packer, AlignmentType, Document, HeadingLevel, Paragraph, TabStopPosition, TabStopType, TextRun } from 'docx';
+import {Packer, AlignmentType, PageBorders,Document, HeadingLevel, Paragraph, TabStopPosition, TabStopType, TextRun } from 'docx';
 import {saveAs} from 'file-saver';
 
+//this function generate a document to be downloaded
 export function genDocxWordWithData(fileName: string, textWrote: string){
-    console.log(fileName, textWrote);
-    if(fileName==""){
+    const newDocument = new Document();
+
+    newDocument.addSection({children: buildDataToTheDocument(textWrote),});
+
+    downloadDocx(newDocument,fileName);
+}
+
+//Here the user download the document
+function downloadDocx(newDoc: Document, fileName: string){
+    if(fileName===""){
         fileName="newDoc";
     }
-    const newDoc = new Document();
 
     Packer.toBlob(newDoc).then(blob => {
-      //console.log(blob);
       saveAs(blob, fileName+".docx");
       console.log("Document created successfully");
     });
+}
+
+//This function build the data to the document
+function buildDataToTheDocument(textWrote: string){
+    let data = [];
+    data[0] = new Paragraph({children: [new TextRun("geeeeeee"+textWrote)]})
+    return data;
 }
