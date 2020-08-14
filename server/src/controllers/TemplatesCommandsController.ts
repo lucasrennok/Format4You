caba6e511986843fbfaa026c6a8e83c73c1fc37a
Lucas Rennó Kallás <lucasrk@unifei.edu.br>
2020-08-14 16:14:48 -0300

    api gets

--- server/src/controllers/TemplatesCommandsController.ts
+++ server/src/controllers/TemplatesCommandsController.ts
@@ -3,21 +3,57 @@ import db from "../database/connections";
 
 export default class TemplatesCommandsController{
     async getAllCommands(request: Request, response: Response){
-        const template = request.query;
-        console.log(template);
-        return response.json({});
+        const {template} = request.query;
+
+        const allCommandsSelect = await db('template_commands')
+            .select('command')
+            .where('template','=',template as string);
+        
+        let allCommandsVector = []
+        
+        for(let i=0; i<allCommandsSelect.length; i++){
+            allCommandsVector[i] = allCommandsSelect[i].command;
+        }
+
+        return response.json({allcommands: allCommandsVector});
     }
 
     async getCommandTextStyle(request: Request, response: Response){
-        const template = request.query;
-        console.log(template);
-        return response.json({});
+        const {template, command} = request.query;
+        
+        const textStyleOfCommand = await db('template_commands')
+            .where('template','=', template as string)
+            .where('command', '=', command as string)
+            .join('text_styles', 'template_commands.text_styles_id', '=', 'text_styles.id')
+            .select('text_styles.bold', 'text_styles.font', 'text_styles.size', 'text_styles.italics');
+
+        return response.json(textStyleOfCommand[0]);
     }
 
     async getCommandFormatStyle(request: Request, response: Response){
-        const template = request.query;
-        console.log(template);
-        return response.json({});
+        const {template, command} = request.query;
+        
+        const indentStyleOfCommand = await db('template_commands')
+            .where('template','=', template as string)
+            .where('command', '=', command as string)
+            .join('format_styles', 'template_commands.format_styles_id', '=', 'format_styles.id')
+            .join('indent', 'format_styles.indent_id', '=', 'indent.id')
+            .select('indent.firstLine', 'indent.hanging', 'indent.left', 'indent.right');
+        
+        const spacingStyleOfCommand = await db('template_commands')
+            .where('template','=', template as string)
+            .where('command', '=', command as string)
+            .join('format_styles', 'template_commands.format_styles_id', '=', 'format_styles.id')
+            .join('spacing', 'format_styles.spacing_id', '=', 'spacing.id')
+            .select('spacing.before', 'spacing.after');
+
+        const alignmentStyleOfCommand = await db('template_commands')
+            .where('template','=', template as string)
+            .where('command', '=', command as string)
+            .join('format_styles', 'template_commands.format_styles_id', '=', 'format_styles.id')
+            .select('format_styles.alignment');
+            
+        return response.json({...alignmentStyleOfCommand[0],spacing: spacingStyleOfCommand[0], indent: indentStyleOfCommand[0]});
     }
 
     async create(request: Request, response: Response){
